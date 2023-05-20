63f913135dbe92d746344ac6dcb43da3b07fd21a
Joan Morales <josmoralesp0307@outlook.es>
2023-05-20 16:39:35 -0500

    quit navbar from auth view

--- src/auth/presentation/layout/AuthLayout.vue
+++ src/auth/presentation/layout/AuthLayout.vue
@@ -1,10 +1,8 @@
 <template>
-    <NavBar />
     <RouterView />
 </template>
 
 <script setup lang="ts">
-import NavBar from '@/root/presentation/components/NavBar.vue'
 import { RouterView } from 'vue-router'
 </script>
 
