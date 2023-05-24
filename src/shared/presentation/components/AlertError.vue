bafcd4db343fd781d2ccfcf2f93ce98bb5bc57fc
Joan Morales <josmoralesp0307@outlook.es>
2023-05-23 21:42:19 -0500

    Add alert components for messages

---
+++ src/shared/presentation/components/AlertError.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alert alert-error shadow-lg">
+        <div>
+            <IconCircleX :size="24" />
+            <span>{{ props.message }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IconCircleX } from '@tabler/icons-vue'
+
+const props = defineProps<{
+    message: string
+}>();
+
+</script>
+
+<style scoped></style>
