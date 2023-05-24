eac78d98b867c80063e25f2b37796dfd41c6a961
Joan Morales <josmoralesp0307@outlook.es>
2023-05-23 22:01:41 -0500

    Create adapter for http requests

---
+++ src/root/infraestructure/ApiAdapter.ts
@@ -0,0 +1,41 @@
+import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
+
+class ApiAdapter {
+
+    private axiosInstance: AxiosInstance;
+
+    constructor(baseURL: string){
+        this.axiosInstance = axios.create({
+            baseURL,
+            timeout: 10000,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+
+        try {
+            const response: AxiosResponse<T> = await this.axiosInstance.get<T, any>(url, config)
+            return response.data;
+        } catch (error: any) {
+            throw new Error(`Error in the request GET ${url}: ${error.message} `)
+        }
+
+    }
+
+    public async post<T>(url: string, data?: T, config?: AxiosRequestConfig): Promise<T> {
+        
+        try {
+            const response: AxiosResponse<T> = await this.axiosInstance.post<T, any>(url, data, config);
+            return response.data
+        } catch (error: any) {
+            throw new Error(`Error in the request POST ${url}: ${error.message} `)
+        }
+        
+    }
+
+}
+
+export default ApiAdapter
