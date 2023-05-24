7453efb7d1af2279a1583e91215a09c6c329052b
Joan Morales <josmoralesp0307@outlook.es>
2023-05-23 20:11:55 -0500

    Create view Register

---
+++ src/auth/presentation/components/RegisterForm.vue
@@ -0,0 +1,101 @@
+<template>
+    <form class="grid grid-cols-12 gap-2">
+        <div class="col-span-12">
+            <div class="mb-4 form-control">
+                <label class="label">
+                    <span class="label-text">Your Name</span>
+                </label>
+                <div class="input-group">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        class="input input-bordered w-full"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        class="input input-bordered w-full"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-12 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Email address</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="example@ferrentacar.com"
+                    class="input input-bordered w-full"
+                />
+            </div>
+        </div>
+
+        <div class="col-span-12 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Number Identification</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="example@ferrentacar.com"
+                    class="input input-bordered w-full"
+                />
+            </div>
+        </div>
+
+        <div class="col-span-6 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Birthdate</span>
+                </label>
+                <input
+                    type="date"
+                    placeholder="example@ferrentacar.com"
+                    class="input input-bordered w-full"
+                />
+            </div>
+        </div>
+
+        <div class="col-span-6 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Phone number</span>
+                </label>
+                <input
+                    type="number"
+                    placeholder="310 311 3203"
+                    class="input input-bordered w-full"
+                />
+            </div>
+        </div>
+
+        <div class="col-span-12 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Profile Photo</span>
+                </label>
+                <input type="file" class="file-input file-input-bordered w-full" />
+            </div>
+        </div>
+
+        <div class="col-span-12 mb-4">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Password</span>
+                </label>
+                <input
+                    type="password"
+                    placeholder="**********"
+                    class="input input-bordered w-full"
+                />
+            </div>
+        </div>
+
+        <div class="col-span-12 mt-4">
+            <button class="btn btn-primary btn-block">Create Account</button>
+        </div>
+    </form>
+</template>
---
+++ src/auth/presentation/views/RegisterView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col items-center p-8 gap-4">
+        <div class="card bg-base-300 w-[28rem] shadow-md">
+            <div class="card-body">
+                <h2 class="card-title">Create Account</h2>
+
+                <RegisterForm />
+            </div>
+        </div>
+
+        <div class="card w-[28rem] bg-base-200 shadow-md">
+            <div class="card-body">
+                <h2 class="card-title text-base">
+                    Already have an account?
+                    <RouterLink
+                        class="link link-hover text-base link-primary"
+                        :to="{ name: 'login-user' }"
+                        >Login to account</RouterLink
+                    >
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import RegisterForm from '../components/RegisterForm.vue'
+</script>
+
+<style scoped></style>
