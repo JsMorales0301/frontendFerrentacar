d9b61a761849c4c53bdd1b5759ed076e54d90d4f
Joan Morales <josmoralesp0307@outlook.es>
2023-05-26 12:19:13 -0500

    Create alerts for different messages

---
+++ src/shared/components/AlertError.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="alert alert-error shadow-lg">
+        <div>
+            <IconCircleXFilled :size="24" />
+            <span>{{ props.message }}</span>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { IconCircleXFilled } from '@tabler/icons-vue'
+const props = defineProps<{
+    message: string
+}>()
+</script>
+
+<style scoped></style>
