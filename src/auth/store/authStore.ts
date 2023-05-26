4083910369e731429cfaf950ff475e8deff8f42c
Joan Morales <josmoralesp0307@outlook.es>
2023-05-26 13:00:50 -0500

    create method for user registration

--- src/auth/store/authStore.ts
+++ src/auth/store/authStore.ts
@@ -5,6 +5,7 @@ import { authEndpoints } from '../endpoints/authEndpoints'
 import { AxiosSingleton } from '@/api/AxiosSingleton'
 import type { LoginRequest } from '../interfaces/loginRequest'
 import type { LoginResponse } from '../interfaces/loginResponse'
+import type { RegisterRequest } from '../interfaces/registerRequests';
 
 export const useAuthStore = defineStore('auth', () => {
     const dataUser = ref<LoginResponse>()
@@ -23,5 +24,14 @@ export const useAuthStore = defineStore('auth', () => {
         }
     }
 
-    return { dataUser, loginUser }
+    async function registerUser(data: RegisterRequest) {
+        try {
+            const response = await AxiosSingleton.getInstance().post(authEndpoints.register, data)
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return { dataUser, loginUser, registerUser }
 })
