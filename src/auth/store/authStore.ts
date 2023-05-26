f51c8d2faa7a5c78b3b5b13da7f2293099d643f0
Joan Morales <josmoralesp0307@outlook.es>
2023-05-26 14:37:17 -0500

    Add method fot get data user

--- src/auth/store/authStore.ts
+++ src/auth/store/authStore.ts
@@ -5,7 +5,7 @@ import { authEndpoints } from '../endpoints/authEndpoints'
 import { AxiosSingleton } from '@/api/AxiosSingleton'
 import type { LoginRequest } from '../interfaces/loginRequest'
 import type { LoginResponse } from '../interfaces/loginResponse'
-import type { RegisterRequest } from '../interfaces/registerRequests';
+import type { RegisterRequest } from '../interfaces/registerRequests'
 
 export const useAuthStore = defineStore('auth', () => {
     const dataUser = ref<LoginResponse>()
@@ -17,10 +17,12 @@ export const useAuthStore = defineStore('auth', () => {
                 credentials
             )
             dataUser.value = data
+            await getDataUser(data.email)
+            localStorage.setItem('info-user', JSON.stringify(dataUser.value))
             return status
         } catch (error) {
             console.log(error)
-            return 400;
+            return 400
         }
     }
 
@@ -28,6 +30,17 @@ export const useAuthStore = defineStore('auth', () => {
         try {
             const response = await AxiosSingleton.getInstance().post(authEndpoints.register, data)
             console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function getDataUser(email: string) {
+        try {
+            const { data } = await AxiosSingleton.getInstance().post(authEndpoints.dataUser, {
+                email
+            })
+            localStorage.setItem('dataUser', JSON.stringify(data))
         } catch (error) {
             console.log(error)
         }
