cb248ad2b2ed50b63c9b290aeb68ee7c82605663
Joan Morales <josmoralesp0307@outlook.es>
2023-05-26 13:00:17 -0500

    Create interface for register req

---
+++ src/auth/interfaces/registerRequests.ts
@@ -0,0 +1,11 @@
+export interface RegisterRequest {
+    numeroDeIdentificacion: string;
+    urlFotoPerfil:          string;
+    nombre:                 string;
+    apellido:               string;
+    fechaNacimiento:        string;
+    email:                  string;
+    password:               string;
+    telefono:               string;
+    estado:                 boolean;
+}
