6ea7b7a015d4434ec9797003a43380c4ce86fa63
Joan Morales <josmoralesp0307@outlook.es>
2023-05-26 13:03:27 -0500

    add method for post request

--- src/auth/components/RegisterForm.vue
+++ src/auth/components/RegisterForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="grid grid-cols-12 gap-2">
+    <form @submit.prevent="createNewUser()" class="grid grid-cols-12 gap-2">
         <div class="col-span-12">
             <div class="mb-4 form-control">
                 <label class="label">
@@ -7,11 +7,13 @@
                 </label>
                 <div class="input-group">
                     <input
+                        v-model="dataRegister.nombre"
                         type="text"
                         placeholder="First Name"
                         class="input input-bordered w-full"
                     />
                     <input
+                        v-model="dataRegister.apellido"
                         type="text"
                         placeholder="Last Name"
                         class="input input-bordered w-full"
@@ -26,6 +28,7 @@
                     <span class="label-text">Email address</span>
                 </label>
                 <input
+                    v-model="dataRegister.email"
                     type="text"
                     placeholder="example@ferrentacar.com"
                     class="input input-bordered w-full"
@@ -39,6 +42,7 @@
                     <span class="label-text">Number Identification</span>
                 </label>
                 <input
+                    v-model="dataRegister.numeroDeIdentificacion"
                     type="text"
                     placeholder="example@ferrentacar.com"
                     class="input input-bordered w-full"
@@ -52,6 +56,7 @@
                     <span class="label-text">Birthdate</span>
                 </label>
                 <input
+                    v-model="unconvertedDate" 
                     type="date"
                     placeholder="example@ferrentacar.com"
                     class="input input-bordered w-full"
@@ -65,7 +70,8 @@
                     <span class="label-text">Phone number</span>
                 </label>
                 <input
-                    type="number"
+                    v-model="dataRegister.telefono"
+                    type="text"
                     placeholder="310 311 3203"
                     class="input input-bordered w-full"
                 />
@@ -77,7 +83,7 @@
                 <label class="label">
                     <span class="label-text">Profile Photo</span>
                 </label>
-                <input type="file" class="file-input file-input-bordered w-full" />
+                <input type="file" class="file-input file-input-bordered w-full" @change="uploadImage" />
             </div>
         </div>
 
@@ -87,6 +93,7 @@
                     <span class="label-text">Password</span>
                 </label>
                 <input
+                    v-model="dataRegister.password"
                     type="password"
                     placeholder="**********"
                     class="input input-bordered w-full"
@@ -95,7 +102,58 @@
         </div>
 
         <div class="col-span-12 mt-4">
-            <button class="btn btn-primary btn-block">Create Account</button>
+            <button type="submit" class="btn btn-primary btn-block">Create Account</button>
         </div>
     </form>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { RegisterRequest } from '../interfaces/registerRequests';
+import { AxiosCloudinarySingleton } from '../../api/cloudinary';
+
+import { useAuthStore } from '../store/authStore';
+
+const dataRegister = ref<RegisterRequest>({
+    numeroDeIdentificacion: '',
+    urlFotoPerfil:          '',
+    nombre:                 '',
+    apellido:               '',
+    fechaNacimiento:        '',
+    email:                  '',
+    password:               '',
+    telefono:               '',
+    estado:                 true,
+})
+
+const unconvertedDate = ref()
+const imageProfile = ref()
+const CLOUDINARY_PRESET_PROFILE = 'profile-ferrentacar';
+const auth = useAuthStore()
+
+const uploadImage = (event: any) => {
+    imageProfile.value = event.target.files[0]
+}
+
+const dateFormatISO = (date: any) => {
+    const fecha = new Date(date)
+    const fechaISO = fecha.toISOString();
+    return fechaISO
+}
+
+const saveImageCloudinary = async() => {
+    const formData = new FormData();
+    formData.append('upload_preset', CLOUDINARY_PRESET_PROFILE);
+    formData.append('file', imageProfile.value)
+    const { data } = await AxiosCloudinarySingleton.getInstance().post('/upload', formData)
+    dataRegister.value.urlFotoPerfil = data.secure_url
+}
+
+const createNewUser = async() => {
+    await saveImageCloudinary()
+    dataRegister.value.fechaNacimiento = dateFormatISO(unconvertedDate.value);
+    await auth.registerUser(dataRegister.value)
+}
+
+
+</script>
